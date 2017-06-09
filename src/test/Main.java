d6180e0ced38783e12ae7517267f5bc0da7c9bdf
nianhuo5678 <nianhuo5678@163.com>
2017-06-09 16:19:49 +0800

    Finished

--- src/test/Main.java
+++ src/test/Main.java
@@ -9,7 +9,11 @@ public class Main {
 		int redWin = 6; //红球中奖个数
 		int blueWin = 1;  //蓝球中奖个数
 		LotteryPlan lp = new LotteryPlan();
-		lp.genPlans(redWin,blueWin);
+		for(redWin = 0; redWin <= 6; redWin++) {
+			for(blueWin= 0; blueWin <= 1; blueWin++){
+				lp.genPlans(redWin,blueWin);
+			}
+		}
 	}
 	
 }
